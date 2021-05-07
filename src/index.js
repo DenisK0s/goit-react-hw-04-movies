476354affd0f023d5fcae0606b8e0d63ffbc6570
DenisK0s <kostenkoden89@gmail.com>
2021-05-07 19:00:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
-  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
